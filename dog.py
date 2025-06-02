8f85add48f76ee6642d46ff323f43b6a1c0278bf
Mahender Bollam <mahender.bollam@mable.com.au>
2025-06-02 15:54:42 +0530

    Initial commit

---
+++ dog.py
@@ -0,0 +1,9 @@
+class Dog:
+    def eat(self):
+        print("I can Eat")
+
+    def sleep(self):
+        print("I can Sleep")
+
+    def bark(self):
+        print("I can Bark")
---
+++ main.py
@@ -0,0 +1,10 @@
+from dog import Dog
+
+
+class Main:
+    
+    # Here DOG animal is created
+    Dog = Dog()
+    Dog.bark()
+    Dog.eat()
+    Dog.sleep()
